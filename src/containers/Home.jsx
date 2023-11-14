b6703c667993fd2a99230b3c1d556cce6eef47e1
Jarez25 <45266911+Jarez25@users.noreply.github.com>
2023-11-14 14:36:47 -0600

    agregando django a react layout & footer

---
+++ src/containers/Home.jsx
@@ -0,0 +1,13 @@
+import Layout from "../hocs/Layout"
+
+export const Home = () => {
+    return(
+        <Layout>
+            <div className="text-blue-500">
+                home
+            </div>
+        </Layout>
+    )
+}
+
+export default Home
