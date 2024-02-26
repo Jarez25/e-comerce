fd05be6ae7868afc659eb848e573d8af5c32d1b6
Jarez25 <45266911+Jarez25@users.noreply.github.com>
2024-02-26 07:52:16 -0600

    Revert "se trata de hacer la conecion con la api del post man"
    This reverts commit 7cd1bce2391761d07ed172c2084ea157fc15d140.

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -10,7 +10,5 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email','is_staff','is_superuser','is_active','last_login' )
     list_display_links = ('first_name', 'last_name', 'email', )
     search_fields = ('first_name', 'last_name', 'email','is_staff','is_superuser','is_active','last_login' )
-    list_per_page = 25
-    
     
 admin.site.register(User, UserAdmin)
