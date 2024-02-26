7cd1bce2391761d07ed172c2084ea157fc15d140
Jarez25 <45266911+Jarez25@users.noreply.github.com>
2024-02-25 22:44:17 -0600

    se trata de hacer la conecion con la api del post man

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -1,2 +1,38 @@
-export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
-export const SIGNUP_FAIL = 'SIGNUP_FAIL';
+import{
+    SIGNUP_SUCCESS,
+    SIGNUP_FAIL,
+}from './types.js'
+
+import axios  from 'axios';
+
+export const signup = (first_name, last_name, email, password, re_password) => async dispatch => {
+    const config = {
+        headers:{
+            'content-Type': 'application/json'
+        }
+    }
+
+    const body = JSON.stringify({
+        first_name,
+        last_name,
+        email,
+        password,
+        re_password
+    });
+
+    try{
+        const res =  await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/`, body, config)
+        
+        if(res.status ===201){
+            dispatch({
+                type: SIGNUP_SUCCESS,
+                payload: res.data
+            })
+        }else {
+            dispatch({
+                type: SIGNUP_FAIL
+            })
+        }
+    }
+    catch(err){}
+}
